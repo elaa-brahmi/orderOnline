3469d984894998bd4bfe8dab2a61aa6295c0900f
elaa <elaa.brahmii@gmail.com>
2024-09-17 21:21:37 +0100

    n

--- resources/views/shared/success-msg.blade.php
+++ resources/views/shared/success-msg.blade.php
@@ -2,6 +2,7 @@
 <div class="alert alert-success">
     {{ session('success') }}
 </div>
+@endif
 @if(session()->has('successOrder'))
 <div class="alert alert-success">
     {{ session('successOrder') }}
