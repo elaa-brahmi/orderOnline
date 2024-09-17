35939a0fcacd33f46a0090aa73c22354d60c7d57
elaa <elaa.brahmii@gmail.com>
2024-09-17 18:57:15 +0100

    n

---
+++ resources/views/authentification.blade.php
@@ -0,0 +1,19 @@
+<?php
+session_start();?>
+@extends('layouts.layout')
+@section("content")
+<div class="col-md-6 mx-auto">
+<form method="POST" action="{{ route("auth") }}">
+    @csrf
+    <br>
+    <label for="login">login</label>
+    <input type="text" name="login" id="login" class="form-control"><br>
+    <label for="password">password</label>
+    <input type="password" name="password" id="password" class="form-control"><br>
+    <input type="submit" value="login" class="btn btn-primary"></form>
+
+</div>
+@endsection
+
+
+
