1832363ecc199d5f4061382ae61b8e77d6e1d2b0
elaa <elaa.brahmii@gmail.com>
2024-09-17 21:20:03 +0100

    n

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Models\client;
-use App\Models\commande;
-use App\Models\ligne_commande;
+use App\Models\Client;
+use App\Models\Commande;
+use App\Models\LigneCommande;
 class orderController extends Controller
 {
     public function order(Request $request){
@@ -13,32 +13,30 @@ class orderController extends Controller
             'phone'=>'required',
             'fname'=>'required',
             'lname'=>'required',
-            'adress'=>'required',
+            'town'=>'required',
             'zipcode'=>'required',
         ]);
-        $order=new client();
-        $order->mail=$validateddata['mail'];
-        $order->phone=$validateddata['phone'];
-        $order->name=$validateddata['fname'];
-        $order->lastname=$validateddata['lname'];
-        $order->adress=$validateddata['adress'];
-        $order->zipcode=$validateddata['zipcode'];
-        $order->save();
+        $client=new Client();
+        $client->mail=$validateddata['mail'];
+        $client->phone=$validateddata['phone'];
+        $client->name=$validateddata['fname'];
+        $client->lastname=$validateddata['lname'];
+        $client->adress=$validateddata['town'];
+        $client->zipcode=$validateddata['zipcode'];
+        $client->save();
         $clientId= $client->idclient;
-        $commande=new commande();
+        $commande=new Commande();
         $commande->idclient=$clientId ;
         $commande->save();
         $cart = json_decode($request->input('cart'), true);
         foreach($cart as $item){
-            $ligne_commande=new ligne_commande();
+            $ligne_commande=new LigneCommande();
+            $ligne_commande->idcommande=$commande->idcommande;
             $ligne_commande->idfood=$item['id_food'];
             $ligne_commande->price=$item['price'];
             $ligne_commande->food_name=$item['food_name'];
-
             $ligne_commande->save();
         }
-        return redirect()->rout('backtodashboard')->with('successOrder','order placed successfully!');
-
-
+        return redirect()->route('backtodashboard')->with('successOrder','order placed successfully!');
     }
 }
