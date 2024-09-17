63d0728ea4cf1c1fe628d8eba3a915a81c99bc12
elaa <elaa.brahmii@gmail.com>
2024-09-17 21:21:49 +0100

    n

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\foodController;
+use App\Http\Controllers\adminController;
+use App\Http\Controllers\orderController;
 
-Route::get('/', function () {
+
+
+Route::get('/dashboard', function () {
     return view('dashboard');
-});
-
+})->name("backtodashboard");
 Route::get('/foods/{category}', [foodController::class, 'showbycategory'])->name('food.category');
 Route::get('/foods/{food_id}', [foodController::class, 'showbyid'])->name('food.id');
+Route::get('/authentification',function(){
+    return view("authentification");
+})->name("authentification");
+Route::post('/dashboard',[adminController::class,'authen'])->name("auth");
+Route::get('/dashboard/checkOrder',function(){
+    return view("order");
+})->name('checkOrder');
+Route::post('/dashboard/checkOrder',[orderController::class,'order'])->name("confirmOrder");
 
 
 
