14c4707e608a9e1ccfd50de9302584b88629a308
elaa <elaa.brahmii@gmail.com>
2024-09-23 21:57:38 +0100

    ee

--- app/Models/LigneCommande.php
+++ app/Models/LigneCommande.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ligne_commande extends Model
+class LigneCommande extends Model
 {
     use HasFactory;
     protected $fillable=['idcommande','idfood','price','quantity'];
