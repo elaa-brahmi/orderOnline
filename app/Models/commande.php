92d77377aee72a4a3e1173e64029d2b7709b40cf
elaa <elaa.brahmii@gmail.com>
2024-09-23 21:57:19 +0100

    e

--- app/Models/commande.php
+++ app/Models/commande.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class commande extends Model
+class Commande extends Model
 {
     use HasFactory;
     protected $primarykey='	idcommande';
